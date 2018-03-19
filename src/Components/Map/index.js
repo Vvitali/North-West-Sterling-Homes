3a3f4ec3424ff16d55158fccf7d720812b4f1967
Vvitali <vvitali26@gmail.com>
2018-03-18 20:05:48 -0400

    map added

---
+++ src/Components/Map/index.js
@@ -0,0 +1,45 @@
+import React from 'react'; 
+import GoogleMapReact from 'google-map-react';
+import MapObject from "./object.js"
+const places = [
+{
+	name: "Portland, OR",
+	position: { lat: 45.570655, lng: -122.799300 },
+	title: "I lived here for 2 years",
+	pic: "http://greatruns.com/wp-content/uploads/2016/11/Mt-Hood-and-Portland-waterfront-Agape-Voyage-700x447.jpeg"
+}
+];
+
+class MapComponent extends React.Component{
+	static defaultProps = {
+		zoom: 10,
+		center: { 
+			lat: 45.5, 
+			lng: -122.7
+		}
+	}
+	componentStyle={ 
+		width: this.props.width, 
+		height: this.props.height,
+		marging: "20px",
+		position: "relative"
+	};
+
+	render(){
+		let renderedPlaces = [];
+		for(let place of places){
+			renderedPlaces.push(<MapObject key={place.name} name={place.name} lat={ place.position.lat } lng={place.position.lng } pic={ place.pic} text={ place.title }/>); 
+		};
+		return (
+			<GoogleMapReact style={this.componentStyle} defaultCenter={ this.props.center } defaultZoom={ this.props.zoom } bootstrapURLKeys={{
+				key: "AIzaSyDyePBmEMRe0sGjQBRvDq8TYEcXkDfp_0I"
+			}}>
+			{
+				renderedPlaces
+			}
+			</GoogleMapReact>
+			);
+	}
+
+}
+export default MapComponent;
--- src/routes/Home/index.js
+++ src/routes/Home/index.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import Parallax from "../../Components/Parallax";
+
+import MapComponent from "../../Components/Map";
 class Home extends Component{
 	render(){  
 		return(
@@ -8,11 +10,15 @@ class Home extends Component{
 			<Parallax linkTo="./test/1.jpg">
 			<h1>Main screen</h1></Parallax>
 			<div className="info"></div>
-			<Parallax linkTo="./test/2.jpg"></Parallax>
+			<Parallax linkTo="./test/2.jpg">
+			<h1>Second main screen</h1></Parallax>
 			<div className="info"></div>
-			<Parallax linkTo="./test/3.jpg"></Parallax>
-			<div className="info"></div>
-			<Parallax linkTo="./test/4.jpg"></Parallax>
+			<Parallax linkTo="./test/3.jpg">
+			<h1>Third Main screen</h1></Parallax>
+			<div className="info">
+			<MapComponent width="70%" height="100%"/></div>
+			<Parallax linkTo="./test/4.jpg">
+			<h1>Fourth Main screen</h1></Parallax>
 			<div className="info">Contacts</div>
 			</div> 
 			)
