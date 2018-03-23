e169ae6bdaf53bbeb8b023a7fbd732fe5b949f62
Vvitali <vvitali26@gmail.com>
2018-03-23 12:05:54 -0400

    header

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -1,42 +1,42 @@
 import React from 'react';
 import "./style.css";
 const Header = ()=>{
-	return(<div>
-		<a name="anchor"></a>
-		<nav className="navbar navbar-default menu">
+return(<div>
+	<a name="anchor"></a>
+	<nav className="navbar navbar-default menu">
 		<div className="container-fluid"> 
-		<div className="navbar-header">
-		<button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-		<span className="sr-only">Toggle navigation</span>
-		<span className="icon-bar"></span>
-		<span className="icon-bar"></span>
-		<span className="icon-bar"></span>
-		</button>
-		<a className="navbar-brand" href="./">NV Sterling Homes</a>
-		</div>
+			<div className="navbar-header">
+				<button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+					<span className="sr-only">Toggle navigation</span>
+					<span className="icon-bar"></span>
+					<span className="icon-bar"></span>
+					<span className="icon-bar"></span>
+				</button>
+				<a className="navbar-brand" href="./">NV Sterling Homes</a>
+			</div>
 
 
-		<div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+			<div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
 
 
-		<ul className="nav navbar-nav navbar-right">
-		<li><a href="./portfolio">Projects</a></li>
-		<li className="dropdown">
-		<a href="#" className="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Services <span className="caret"></span></a>
-		<ul className="dropdown-menu">
-		<li><a href="#">Masonry</a></li>
-		<li><a href="#">Painting</a></li>
-		<li><a href="#">Something else here</a></li>
-		<li role="separator" className="divider"></li>
-		<li><a href="#">Separated link</a></li>
-		</ul>
-		</li>
-		<li><a href="./contact">Contacts</a></li>
-		</ul>
+				<ul className="nav navbar-nav navbar-right">
+					<li><a href="./portfolio">Projects</a></li>
+					<li className="dropdown">
+						<a href="#" className="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Services <span className="caret"></span></a>
+						<ul className="dropdown-menu">
+							<li><a href="#">Masonry</a></li>
+							<li><a href="#">Painting</a></li>
+							<li><a href="#">Something else here</a></li>
+							<li role="separator" className="divider"></li>
+							<li><a href="#">Separated link</a></li>
+						</ul>
+					</li>
+					<li><a href="./contact">Contacts</a></li>
+				</ul>
+			</div> 
 		</div> 
-		</div> 
-		</nav>
+	</nav>
 
-		</div>);
+</div>);
 }
 export default Header;
--- src/routes/Home/index.js
+++ src/routes/Home/index.js
@@ -1,60 +1,63 @@
 import React, { Component } from 'react';
 import Parallax from "../../Components/Parallax";
-import {isMobile} from 'react-device-detect'
+import {isMobile} from 'react-device-detect';
 import MapComponent from "../../Components/Map";
 import Slider from "../../Components/Slider";
 import "./styles.css"
 
 class Home extends Component{
-	render(){  
-		return(
+render(){  
+return(
 
-			<div className="BioStyle ">
-			<div className="row">
-			<Slider></Slider>
-			</div>
-			<div className="row info">
+<div className="BioStyle ">
+	<div className="row">
+		<Slider></Slider>
+	</div>
+	<div className="row info">
 
-			</div>
-			<div className="row">
-			<Parallax height={isMobile? "350px": "750px"} linkTo="./test/2.jpg">
+	</div>
+	<div className="row">
+		<Parallax height={isMobile? "350px": "750px"} linkTo="./test/2.jpg">
 			<h1>Second main screen</h1>
-			</Parallax>
-			</div>
-			
-			<div className="row info">
+		</Parallax>
+	</div>
 
-			</div>
-			<Parallax className="row" height={isMobile? "350px": "750px"} linkTo="./test/3.jpg">
+	<div className="row info">
+
+	</div>
+	<div className="row">
+		<Parallax className="row" height={isMobile? "350px": "750px"} linkTo="./test/3.jpg">
 			<h1>Third Main screen</h1>
-			</Parallax>
-			<div className="row info">
-			<div className="col-xs-12 col-sm-12 col-md-4 map" >
+		</Parallax>
+	</div>
+	<div className="row info">
+		<div className="col-xs-12 col-sm-12 col-md-4 map" >
 			<MapComponent width="100%" height="100%">
 
 			</MapComponent>
-			</div>
+		</div>
 
-			<div className="col-xs-12 col-sm-12 col-md-4 contacts">
+		<div className="col-xs-12 col-sm-12 col-md-4 contacts">
 			<h1>Phone-number (clickable):</h1>
 			<p>
-			<a className="btn pulse" href="tel:+15037400866">+1(503)-740-08-66</a>
+				<a className="btn pulse" href="tel:+15037400866">+1(503)-740-08-66</a>
 			</p>
 			<h1>Email (clickable):</h1>
 			<a className="btn pulse" href="mailto: nwsterlinghomes@gmail.com">nwsterlinghomes@gmail.com</a>
-			</div>
-			</div>
-			<div className="row">
-			<div className="col-xs-12">
-			<Parallax height={isMobile? "350px": "750px"} className="row" linkTo="./test/4.jpg">
-			<h1>Fourth Main screen</h1>
+		</div>
+	</div>
+	<div className="row">
+		<div className="col-xs-12">
+			<Parallax height={isMobile? "350px": "750px"}  linkTo="./test/4.jpg">
+				<h1>Fourth Main screen</h1>
 			</Parallax>
-			</div>
-			</div>
-			<div className="row info">Contacts
-			</div>
-			</div> 
-			)
-	};
+		</div>
+
+	</div>
+	<div className="row info">Contacts
+	</div>
+</div> 
+)
+};
 }
 export default Home;
