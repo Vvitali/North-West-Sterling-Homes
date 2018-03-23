30a019bfdbbe772f94a4e89629434964a8995fdd
Vvitali <vvitali26@gmail.com>
2018-03-23 13:48:40 -0400

    chat - changed to defer instead of asycn

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -15,7 +15,6 @@ return(<div>
 				<a className="navbar-brand" href="./">NV Sterling Homes</a>
 			</div>
 
-
 			<div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
 				<ul className="nav navbar-nav navbar-right">
 					<li><a href="./portfolio">Projects</a></li>
