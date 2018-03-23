ec48e1eeaf97f893a03a5a6e9644a88dd6e330ad
Vvitali <vvitali26@gmail.com>
2018-03-23 12:22:40 -0400

    arrows disabled on slider

--- src/Components/Slider/index.js
+++ src/Components/Slider/index.js
@@ -4,33 +4,34 @@ import "./slider.css";
 import Parallax from "../Parallax"; 
 import {isMobile} from 'react-device-detect';
 const SliderComponent = (props)=>{
-var settings = {
-dots: true,
-infinite: true,
-speed: 500,
-slidesToShow: 1,
-slidesToScroll: 1,
-autoplay: true,
-autoplaySpeed: 2000
-};
-return (
-<Slider {...settings}>
-	<div>
+	var settings = {
+		dots: true,
+		infinite: true,
+		speed: 500,
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		autoplay: true,
+		autoplaySpeed: 2000,
+		arrows: false
+	};
+	return (
+		<Slider {...settings}>
+		<div>
 		<Parallax height={isMobile? "350px": "750px"} linkTo="./test/1.jpg">
-			<h1>Main screen</h1>
+		<h1>Main screen</h1>
 		</Parallax>
-	</div>
-	<div>
+		</div>
+		<div>
 		<Parallax height={isMobile? "350px": "750px"} linkTo="./test/2.jpg">
-			<h1>Main screen</h1>
+		<h1>Main screen</h1>
 		</Parallax>
-	</div>
-	<div>
+		</div>
+		<div>
 		<Parallax height={isMobile? "350px": "750px"} linkTo="./test/3.jpg">
-			<h1>Main screen</h1>
+		<h1>Main screen</h1>
 		</Parallax>
-	</div>
-</Slider>
-)
+		</div>
+		</Slider>
+		)
 }
 export default SliderComponent;
