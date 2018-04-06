2011bcad9c2d08ccf3710d651c00004306ee7f22
Vvitali <vvitali26@gmail.com>
2018-04-06 19:03:22 -0400

    restructured isMobile style switch

--- src/Components/ProjectItem/index.js
+++ src/Components/ProjectItem/index.js
@@ -2,23 +2,34 @@ import React from "react";
 import {isMobile} from 'react-device-detect';
 
 const ProjectItem = (props)=>{
-return (
-<div style={{ width: "100%"}}>
-	<img src={props.img} style={{ width: isMobile? props.width: (props.width/3)}}/>
-	<div></div>
-	<div className="info" style={{height: isMobile? "250px": (props.width/3)}}>
+	return (
+		<div style={{ width: "100%"}}>
+		<img src={props.img} style={
+			isMobile?{
+				width:props.width,
+				float:"none"
+			}:{
+				width:(props.width/3),
+				float:"left"
+			}
+		}/> 
+		<div className="info" style={
+			{
+				height: isMobile? "250px": (props.width/3)
+			}
+		}>
 		{
 			React.Children.map(props.children, (child, i) => {
-			if(child != null){
-			return child;
-		}else{
-		return 
-	}
-})
-}
-</div>
-</div>
-)
+				if(child != null){
+					return child;
+				}else{
+					return 
+				}
+			})
+		}
+		</div>
+		</div>
+		)
 }
 
 export default ProjectItem;
